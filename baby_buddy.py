fb796c9068f86835651a9cd246633c4b32d26510
Robert Greiner <robert@robertgreiner.com>
2014-09-13 12:43:52 -0500

    Call Azure API

--- baby_buddy.py
+++ baby_buddy.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
+import urllib
 
-sensorPin = 7
+device_id = "1000"
+
+def fetch_url(url, params, method): 
+    result = urllib.urlopen(url, params)
+    print result.code
+    print result.read()
+
+def write_motion_sample():
+    #fetch_url("http://babybuddy.cloudapp.net/api/motions", "1000", "POST")
+    fetch_url("http://babybuddy.cloudapp.net/api/motions?deviceId=" + device_id, device_id.encode("ASCII"), "POST")
+
+
+sensor_pin = 7
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(sensorPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-currState = False
+motion_detected = False
+
 
 while True:
     time.sleep(0.1)
-    currState = GPIO.input(sensorPin)
+    motion_detected = GPIO.input(sensor_pin)
    
-    if currState:
-        print "MOTION DETECTED!"
+    if motion_detected:
+        write_motion_sample()
         time.sleep(1)
